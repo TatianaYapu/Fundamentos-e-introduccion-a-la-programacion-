926d8070417261527610500570f2b66f80048105
TatianaYapu <137810230+TatianaYapu@users.noreply.github.com>
2023-06-26 11:50:38 -0400

    Add files via upload
    primercommit

---
+++ prueba/Ejemplo.java
@@ -0,0 +1,21 @@
+
+public class Ejemplo 
+{
+    // Espacio donde se declaran todos los aributos
+    private int numero;
+
+    /**
+     * Constructor de la clase
+     */
+    public Ejemplo(int valor){
+        numero=valor;
+    }
+
+    /**
+     * Metodo,algo que saben hacer los objetos de esta clase
+     */
+    public int sampleMethod()
+    {
+        return numero;
+    }
+}

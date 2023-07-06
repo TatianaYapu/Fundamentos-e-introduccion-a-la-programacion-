d8f8983f3231612ca02a8ac61fc34811194b6877
TatianaYapu <yapuzaratetatiana@gmail.com>
2023-07-06 00:38:58 -0400

    ejemplomatries

---
+++ ejemplomatrices/Recorridos.java
@@ -0,0 +1,48 @@
+
+public class Recorridos{
+    public void diagonalPrincipal(int tam){
+        int[][] matriz= new int[tam][tam];
+        for(int i=0;i<tam;i++){
+            for(int j=0;j<tam;j++){
+                if(i==j){
+                matriz[i][j]=1;  
+                }
+            }
+        }
+        
+        mostrar(matriz);
+    } 
+    public void diagonalSeundaria(int tam){
+        int[][] matriz= new int[tam][tam];
+        for(int i=0;i<tam;i++){
+            for(int j=0;j<tam;j++){
+                if((i+j)==tam-1){
+                   matriz[i][j]=1;    
+                }
+            }
+        
+        mostrar(matriz);
+         }
+    } 
+    public void ajedrez(int tam){
+        int[][] matriz= new int[tam][tam];
+        for(int i=0;i<tam;i++){
+            for(int j=0;j<tam;j++){
+               if((i+j)%2==0){
+                   matriz[i][j]=1;
+               }
+            }
+        }
+        mostrar(matriz);
+    }
+    public void mostrar(int[][]mat){
+        for(int fila=0;fila<mat.length;fila++){
+            for(int col=0;col<mat[fila].length;col++){
+                System.out.print(mat[col][fila]+"  ");
+             
+            }
+            System.out.println();
+        }
+    
+    }
+}
